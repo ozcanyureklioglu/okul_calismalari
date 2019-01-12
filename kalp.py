6b8bce6c0553bb8bbd97bb3487e0470d6ad228b1
Ozcan Yureklioglu <ozcanyureklioglu@Ozcan-MacBook-Air.local>
2019-01-12 15:59:06 +0300

    python console heart

---
+++ kalp.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+#author Ozcan YUREKLIOGLU
+
+a=0
+b=0
+for i in range(7,24,4):
+    print(" "*(9-a),end='')
+    print("♥"*i,end='')
+    a=a+2
+    
+    print(" "*(25-i),end='')
+    print("♥"*i,end='')
+    print()
+print("♥"*50)
+print("♥"*50)
+print("♥"*19,"I LOVE YOU","♥"*19)
+print("♥"*50)
+print("♥"*50)
+for j in range(50,-1,-4):
+    print(" "*(b),end='')
+    print("♥"*(j))
+    b=b+2
+
+    
+    
+    
