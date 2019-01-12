afad1358ea44bd9be3c427e31ea32a16a452e46c
Ozcan Yureklioglu <ozcanyureklioglu@Ozcan-MacBook-Air.local>
2019-01-12 15:52:20 +0300

    matplotlib.pyplot modul

---
+++ plot_deneme.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 10 14:28:37 2018
+
+@author: ozcanyureklioglu
+"""
+import matplotlib.pyplot as plt
+
+
+plt.plot((2,3,4),(2,3,5),color="blue")
+
+
+
